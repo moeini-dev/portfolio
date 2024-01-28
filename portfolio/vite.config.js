b301723b939c181a782a7c939f44f775cace85df
moeini-dev <moeini.dev@gmail.com>
2024-01-28 13:59:20 +0330

    Edit base to test if it works

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -5,10 +5,5 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+  base: '/portfolio/portfolio/'
 })
