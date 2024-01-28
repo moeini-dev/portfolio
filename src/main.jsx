aed811593f70d71b4b04b4a5ec6f030ec48ff5fb
moeini-dev <moeini.dev@gmail.com>
2024-01-28 17:19:52 +0330

    Comment out router

--- src/main.jsx
+++ src/main.jsx
@@ -11,19 +11,21 @@ import './index.css';
 import Home from './pages/Home.jsx';
 import ErrorPage from './pages/ErrorPage.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-    errorElement: <ErrorPage />
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Home />,
+//     errorElement: <ErrorPage />
+//   }
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
     
-    <RouterProvider router={router}/>
+    {/* <RouterProvider router={router}/> */}
+
+    <Home />
 
   </React.StrictMode>,
 )
